af2857beb6e0a875b00edb62c89f4ea6e7fb92a2
talha <talhairfan2015@gmail.com>
2025-02-18 17:05:45 +0500

    changes applied

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter , RouterProvider} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import App from './App.jsx'
 import About from './about.jsx'
 import Work from './work'
