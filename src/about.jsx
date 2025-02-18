ce3de34aab91392a4f76c020eaf95d95988aed66
talha <talhairfan2015@gmail.com>
2025-02-18 17:10:27 +0500

    changed

--- src/about.jsx
+++ src/about.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import './style.css'
-import Icon from "./assets/Icon.png"
+import Icon from "./assets/icon.png"
 import Cursor from "./cursor"
 import {Link} from 'react-router-dom'
 
